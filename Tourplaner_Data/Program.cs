d7eac0a4c7a0737cadcb3835b4953fbae58a8419
if19b097 <if19b097@technikum-wien.at>
2021-03-14 14:11:41 +0100

    Created Projects for different layers

---
+++ Tourplaner_Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tourplaner_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
